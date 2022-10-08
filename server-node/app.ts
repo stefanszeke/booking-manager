cc3d3f63ee910bd877161c3187b0f6b33e6cdbaf
Stefan Szekely <sekestefan@gmail.com>
2022-10-08 08:28:51 +0200

    build: frontend

--- server-node/app.ts
+++ server-node/app.ts
@@ -1,18 +1,20 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 
+
 import { calendarRouter } from "./routes/booking";
 
 const app = express();
 app.use(express.json());
 app.use(cors())
+app.use(express.static('public'));
 
 //routes
 app.use('/api/booking', calendarRouter);
 
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+app.get("*", (req: Request, res: Response) => {
+  res.redirect("/");
 })
 
 const PORT = process.env.PORT || 3700;
