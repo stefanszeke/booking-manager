7a1b6119ed610581c20ae53fba31e3a4ea8ae523
Stefan Szekely <sekestefan@gmail.com>
2022-10-11 16:56:43 +0200

    trying to fix new build deployment 3

--- server-node/app.ts
+++ server-node/app.ts
@@ -16,7 +16,7 @@ app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.get("test", (req: Request, res: Response) => {
+app.get("/test", (req: Request, res: Response) => {
     res.send("test");
 });
 
